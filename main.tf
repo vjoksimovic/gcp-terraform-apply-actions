59d6759fdd312ca1a45b01f78d779b4e9f3e67fa
vjoksimovic <vjoksimovic@griddynamics.com>
2023-03-31 14:06:09 +0200

    change name

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_storage_bucket" "bucket" {
-  name     = "test-veljo-1"
+  name     = "test-veljko-1"
   location = "europe-west2"
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name     = "test-veljo-2"
+  name     = "test-veljko-2"
   location = "europe-west2"
 }
